3d4d842cb120ad6cefb0104bc046988368615b7f
Rohit kumar barada <rohitbarada@gmail.com>
2023-07-02 16:14:14 +0530

    commit

--- src/config.js
+++ src/config.js
@@ -1,22 +1,22 @@
-// const config = {
-//   MAX_ATTACHMENT_SIZE: 5000000,
-//     s3: {
-//       REGION: "us-east-1",
-//       BUCKET: "rohit-notes",
-//     },
-//     apiGateway: {
-//       REGION: "us-east-1 ",
-//       URL: "https://e9kxoyawyi.execute-api.us-east-1.amazonaws.com/prod",
-//     },
-//     cognito: {
-//       REGION: "eu-north-1",
-//       USER_POOL_ID: "eu-north-1_LVSQYoJxz",
-//       APP_CLIENT_ID: "4nbl8jl29f92al1n4q4jbtabot",
-//       IDENTITY_POOL_ID: "eu-north-1:e3f58571-ba24-4805-9a11-d6c673f3ca09",
-//     },
-//   };
+const config = {
+  MAX_ATTACHMENT_SIZE: 5000000,
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "rohit-notes",
+    },
+    apiGateway: {
+      REGION: "us-east-1 ",
+      URL: "https://e9kxoyawyi.execute-api.us-east-1.amazonaws.com/prod",
+    },
+    cognito: {
+      REGION: "eu-north-1",
+      USER_POOL_ID: "eu-north-1_LVSQYoJxz",
+      APP_CLIENT_ID: "4nbl8jl29f92al1n4q4jbtabot",
+      IDENTITY_POOL_ID: "eu-north-1:e3f58571-ba24-4805-9a11-d6c673f3ca09",
+    },
+  };
   
-//   export default config;
+  export default config;
 // const config = {
 //   MAX_ATTACHMENT_SIZE: 5000000,
 //     s3: {
@@ -36,22 +36,22 @@
 //   };
   
 //   export default config;
-const config = {
-  MAX_ATTACHMENT_SIZE: 5000000,
-  s3: {
-    REGION: "us-east-1",
-    BUCKET: "rohit-notes",
-  },
-  apiGateway: {
-    REGION: "us-east-1",
-    URL: "https://bcifjywz69.execute-api.us-east-1.amazonaws.com/prod",
-},
-cognito: {
-  REGION: "eu-north-1",
-  USER_POOL_ID: "eu-north-1_EbCxHZ5SC",
-  APP_CLIENT_ID: "7ldo2bm9ac8mm1s0038skvje0c",
-  IDENTITY_POOL_ID: "eu-north-1:b6a8e5a5-27b7-489a-b443-576b26360ab5",
-},
+// const config = {
+//   MAX_ATTACHMENT_SIZE: 5000000,
+//   s3: {
+//     REGION: "us-east-1",
+//     BUCKET: "rohit-notes",
+//   },
+//   apiGateway: {
+//     REGION: "us-east-1",
+//     URL: "https://bcifjywz69.execute-api.us-east-1.amazonaws.com/prod",
+// },
+// cognito: {
+//   REGION: "eu-north-1",
+//   USER_POOL_ID: "eu-north-1_EbCxHZ5SC",
+//   APP_CLIENT_ID: "7ldo2bm9ac8mm1s0038skvje0c",
+//   IDENTITY_POOL_ID: "eu-north-1:b6a8e5a5-27b7-489a-b443-576b26360ab5",
+// },
   
-};
-export default config;
+// };
+// export default config;
